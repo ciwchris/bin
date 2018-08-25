5679de50e1e7f5e649f010cc102ab7a49b34e592
Christopher Lopes <christopher.lopes@gmail.com>
2018-08-25 14:35:11 -0700

    Put key mapping in its own script
    So that it can be run separately when a keyboard is plugged in as well
    as when the keyboard layout is toggled.

--- .bin/toggle-keyboard.sh
+++ .bin/toggle-keyboard.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[[ -f $HOME/.bin/modes/current-keyboard-mode ]] && KEYBOARD_LAYOUT=$(<$HOME/.bin/modes/current-keyboard-mode)
+[[ -f $HOME/.bin/current-keyboard-mode ]] && KEYBOARD_LAYOUT=$(<$HOME/.bin/current-keyboard-mode)
 
 if [ -z "$KEYBOARD_LAYOUT" ]; then
     setxkbmap -layout us -variant dvorak
@@ -12,20 +12,6 @@ elif [ "$SOLARIZED_THEME" != "qwerty" ]; then
     setxkbmap -layout us
     KEYBOARD_LAYOUT="qwerty"
 fi
-echo $KEYBOARD_LAYOUT > $HOME/.bin/modes/current-keyboard-mode
+echo $KEYBOARD_LAYOUT > $HOME/.bin/current-keyboard-mode
 
-# Make caps as control and escape at the same time. Based on super-caps by cmatheson.
-xmodmap -e "clear Lock"
-xmodmap -e "keysym Caps_Lock = Control_L"
-xmodmap -e "add Control = Control_L"
-xmodmap -e "add Lock = Caps_Lock"
-xmodmap -e "keycode 999 = Escape"
-
-# # Map the Menu key to the PrtSc key
-xmodmap -e "keysym Print = Menu" 
-
-# # Make the newly mapped Caps Lock key also be used as an Escape key.
-xcape -e "Control_L=Escape"
-
-# # map mute and unmute keys
-xbindkeys &
+$HOME/.bin/set-keymapping.sh
